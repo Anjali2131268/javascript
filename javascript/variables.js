15398916000abcd5e226b8ef7d024fc8898e359d
Anjali <109717001+Anjali2131268@users.noreply.github.com>
2025-03-05 19:59:59 +0000

    variables and hoisting code

---
+++ javascript/variables.js
@@ -0,0 +1,13 @@
+var a= 10;
+// console.log("value of a is:",a);
+var a =20;
+console.log("value of a is:",a);
+let b=75;
+console.log("value of b is:",b);
+ b="anjali";
+console.log("value of b is:",b);
+const c= 55;
+console.log("value of c is:",c);
+const c=78;
+console.log("value of c is:",c);
+
