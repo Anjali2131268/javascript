a7283cf7dfad928dcc2252b18c624b53472712e3
Anjali <109717001+Anjali2131268@users.noreply.github.com>
2025-03-18 20:11:04 +0000

    datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+let name ="anjali";
+let age=18;
+let city;
+let id=null;
+let id2= null;
+let istrueinfo = true;
+let sym1 = Symbol(); 
+let obj={
+    name:"anjali",
+    age:'18'
+}
+console.log( typeof name, typeof age, typeof city, typeof id, typeof istrueinfo ,typeof sym1, typeof obj , id2===null)
+
+
+
