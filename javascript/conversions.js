12b94f923d5d9299b6c89c4804ad990ed7573bdc
Anjali <109717001+Anjali2131268@users.noreply.github.com>
2025-03-22 11:53:44 +0000

    conversion of datatypes

---
+++ javascript/conversions.js
@@ -0,0 +1,18 @@
+let score = "33";
+let typeOfNumIs = Number(score);
+console.log(typeOfNumIs); 
+console.log( typeof typeOfNumIs);
+
+//"33"= 33
+// "33abc" = NAN
+// boolean = 0,1 
+// undefined =NAN
+// null = 0
+
+let IsUserLoggedIn = 1;
+let toCheck = Boolean(IsUserLoggedIn)
+console.log(toCheck);
+console.log(typeof toCheck)
+
+// similarly we ca use string too and other datatypes also
+
